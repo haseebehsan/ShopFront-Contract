514a0157dd1488774af1bf6a244b53b783e83d33
Haseeb Ehsan <haseeb_ehsan@yahoo.com>
2017-09-19 14:29:43 +0500

    added active switch, buy method started
    leaving work in the middle for class, NOTE: continue with buy product method.

--- contracts/Shopfront.sol
+++ contracts/Shopfront.sol
@@ -1,9 +1,34 @@
+/*
+
+--A shopfront
+
+-The project will start as a database whereby:
+
+as an administrator, you can add products, which consist of an id, a price and a stock.
+as a regular user you can buy 1 of the products.
+as the owner you can make payments or withdraw value from the contract.
+
+-Eventually, you will refactor it to include:
+
+ability to remove products.
+co-purchase by different people.
+add merchants akin to what Amazon has become.
+add the ability to pay with a third-party token.
+
+
+
+
+
+
+*/
+
 pragma solidity ^0.4.2;
 
 contract Shopfront {
   address admin;
   mapping(uint => Product) products;
   uint ids;
+  uint balance;
 
 
 
@@ -11,7 +36,7 @@ contract Shopfront {
     uint id;
     uint price;
     uint stock;
-    
+    bool active;
   }
 
 
@@ -31,6 +56,7 @@ contract Shopfront {
  	newProduct.id = ids;
  	newProduct.price = price;
  	newProduct.stock = stock;
+ 	newProduct.active = true;
 
  	products[ids] = newProduct;
  	return newProduct.id;
@@ -46,5 +72,14 @@ contract Shopfront {
 
   function get() constant returns (uint) {
     return storedData;
+  }
+
+  function buyProduct(uint productID, uint stock)
+  payable
+  public{
+  	Product product = products[productID];
+  	if(!product.active) throw; //if product is not active;
+  	if(msg.value < product.price) throw;
+  	if(product.stock <= 0) throw;
   }
 }
